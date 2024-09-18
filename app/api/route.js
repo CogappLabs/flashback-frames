ff23934bf2ea7400d61f198c705a3905ebe1e6f8
Hannah Baker <hannahb@cogapp.com>
2024-09-18 16:44:16 +0100

    Initial project setup

---
+++ app/api/route.js
@@ -0,0 +1,34 @@
+import { NextResponse } from "next/server";
+
+// To handle a GET request to /api
+export async function GET(request) {
+    const countries = ['England', 'France', 'Germany', 'Sweden', 'Norway', 'Finland'];
+
+
+    // Generate a random index within the array length
+    const randomIndex = Math.floor(Math.random() * countries.length);
+
+    // Get the random item from the array
+    const country = countries[randomIndex];
+    const apiKey = process.env.EUROPEANA_API_KEY;
+
+    try {
+        // Store the API response in a variable
+        let response = await fetch(`https://api.europeana.eu/record/v2/search.json?query=photo+or+portrait+of+person&qf=where:${country}&wskey=${apiKey}`);
+
+        // If the call failed, throw an error
+        if (!response.ok) {
+            throw 'Something went wrong.';
+        }
+
+        // Otherwise, get the post JSON
+        let data = await response.json();
+
+        return NextResponse.json({ data }, { status: 200 });
+        // res.status(200).json(data);
+
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }   
+}
