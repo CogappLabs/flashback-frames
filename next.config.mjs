e49067c402d657ad1777565585a96d4a45245859
Hannah Baker <hannahb@cogapp.com>
2026-09-14 15:23:44 +0100

    Complete the Next.js upgrade

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,12 @@
 const nextConfig = {
     reactStrictMode: false,
     images: {
-        domains: ['api.europeana.eu'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'api.europeana.eu',
+            },
+        ],
     },
 };
 
