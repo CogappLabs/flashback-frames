7367938a3457852c79aa6e914013e655d721ced4
Hannah Baker <hannahb@cogapp.com>
2026-09-14 12:55:20 +0100

    Upgrade Next.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,10 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
